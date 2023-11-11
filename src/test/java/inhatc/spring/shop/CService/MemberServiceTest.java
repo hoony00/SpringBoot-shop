1ce7ee4af24988555eae08ce8d99294aa130b463
hoony00 <nasi3611@naver.com>
2023-11-11 23:12:31 +0900

    지연로딩 테스트

--- src/test/java/inhatc/spring/shop/CService/MemberServiceTest.java
+++ src/test/java/inhatc/spring/shop/CService/MemberServiceTest.java
@@ -8,6 +8,7 @@ import inhatc.spring.shop.constant.ItemSellStatus;
 import inhatc.spring.shop.dto.MemberFormDto;
 import inhatc.spring.shop.repository.ItemRepository;
 import inhatc.spring.shop.repository.MemberRepository;
+import inhatc.spring.shop.repository.OrderItemRepository;
 import inhatc.spring.shop.repository.OrderRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityNotFoundException;
@@ -39,6 +40,9 @@ class MemberServiceTest {
 
     @Autowired
     MemberRepository memberRepository;
+
+    @Autowired
+    OrderItemRepository orderItemRepository;
 
     public Item createItem(){
         Item item = new Item();
@@ -101,6 +105,21 @@ class MemberServiceTest {
         Order order = this.createOrder();
         order.getOrderItems().remove(0);
         em.flush();
+    }
+
+    @Test
+    @DisplayName("지연 로딩 테스트")
+    public void lazyLoadingTest(){
+        Order order = this.createOrder();
+        Long orderItemId = order.getOrderItems().get(0).getId();
+        em.flush();
+        em.clear();
+        OrderItem orderItem = orderItemRepository.findById(orderItemId)
+                .orElseThrow(EntityNotFoundException::new);
+        System.out.println("Order class : " + orderItem.getOrder().getClass());
+        System.out.println("===========================");
+        orderItem.getOrder().getOrderDate();
+        System.out.println("===========================");
     }
 
 
