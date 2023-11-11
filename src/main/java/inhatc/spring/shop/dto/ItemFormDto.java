7dffcd6834545e7d60633528105ff61fe904c799
hoony00 <nasi3611@naver.com>
2023-11-11 22:10:19 +0900

    영속성 전이 테스트

--- src/main/java/inhatc/spring/shop/dto/ItemFormDto.java
+++ src/main/java/inhatc/spring/shop/dto/ItemFormDto.java
@@ -36,7 +36,6 @@ public class ItemFormDto {
 
     // private List<ItemImgDto> itemImgDtoList = new ArrayList<>();
 
-    private List<Long> itemImgIds = new ArrayList<>();
 
    // private static ModelMapper modelMapper = new ModelMapper();
 
--- src/test/java/inhatc/spring/shop/CService/MemberServiceTest.java
+++ src/test/java/inhatc/spring/shop/CService/MemberServiceTest.java
@@ -1,13 +1,25 @@
 package inhatc.spring.shop.CService;
 
+import inhatc.spring.shop.Entity.Item;
 import inhatc.spring.shop.Entity.Member;
+import inhatc.spring.shop.Entity.Order;
+import inhatc.spring.shop.Entity.OrderItem;
+import inhatc.spring.shop.constant.ItemSellStatus;
 import inhatc.spring.shop.dto.MemberFormDto;
+import inhatc.spring.shop.repository.ItemRepository;
+import inhatc.spring.shop.repository.OrderRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,35 +28,55 @@ import static org.junit.jupiter.api.Assertions.*;
 class MemberServiceTest {
 
     @Autowired
-    private PasswordEncoder passwordEncoder;
+    OrderRepository orderRepository;
 
     @Autowired
-    private MemberService memberService;
+    ItemRepository itemRepository;
 
-    public Member createMember() {
-        MemberFormDto dto = MemberFormDto.builder()
-                .address("인천시 미추홀구")
-                .email("test@test.com")
-                .name("홍길동")
-                .password("1111")
-                .build();
+    @PersistenceContext
+    EntityManager em;
 
-     return Member.createMember(dto, passwordEncoder);
-
-
+    public Item createItem(){
+        Item item = new Item();
+        item.setItemNm("테스트 상품");
+        item.setPrice(10000);
+        item.setItemDetail("상세설명");
+        item.setItemSellStatus(ItemSellStatus.SELL);
+        item.setStockNumber(100);
+        return item;
     }
 
     @Test
-    void saveMemberTest() {
-        Member member1 = createMember();
-        Member member2 = createMember();
-        memberService.saveMember(member1);
-        System.out.println(member1);
+    @DisplayName("영속성 전이 테스트")
+    public void cascadeTest() {
 
-        Throwable e = assertThrows(IllegalStateException.class, () -> {
-            memberService.saveMember(member2);
-        });
-        Assertions.assertEquals("이미 존재하는 회원입니다", e.getMessage());
+        Order order = new Order();
 
+        for(int i=0;i<3;i++){
+            Item item = this.createItem();
+            itemRepository.save(item);
+            OrderItem orderItem = new OrderItem();
+            // orderItem에 item을 넣어줌
+            orderItem.setItem(item);
+            orderItem.setCount(10);
+            orderItem.setOrderPrice(1000);
+            orderItem.setOrder(order);
+
+            // 자식 엔티티를 부모 엔티티에 추가
+            order.getOrderItems().add(orderItem);
+        }
+
+        // 부모 엔티티를 저장할 때, 연관된 자식 엔티티도 저장됨 (CascadeType.ALL 설정으로)
+        orderRepository.saveAndFlush(order);
+        em.clear();
+
+        Order savedOrder = orderRepository.findById(order.getId())
+                .orElseThrow(() -> new EntityNotFoundException("Order not found"));
+        assertEquals(3, savedOrder.getOrderItems().size());
+
+        System.out.println("OrderItems size: " + savedOrder.getOrderItems().size());
     }
+
+
 }
+
