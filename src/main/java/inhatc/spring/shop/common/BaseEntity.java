c831323993d8c77183815b7e6d785920cd7a39a3
hoony00 <nasi3611@naver.com>
2023-11-14 14:39:15 +0900

    baseEntity

--- src/main/java/inhatc/spring/shop/common/BaseEntity.java
+++ src/main/java/inhatc/spring/shop/common/BaseEntity.java
@@ -1,9 +1,19 @@
 package inhatc.spring.shop.common;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+
+@EntityListeners(value = {AuditingEntityListener.class}) // AuditorAwareImpl.class를 통해 생성, 수정 시간을 자동으로 업데이트
+@MappedSuperclass // 해당 클래스를 상속받는 클래스에게 속성만 제공
+@Getter
+@Setter
 public class BaseEntity extends BaseTimeEntity {
 
     @CreatedBy
--- src/main/java/inhatc/spring/shop/common/BaseTimeEntity.java
+++ src/main/java/inhatc/spring/shop/common/BaseTimeEntity.java
@@ -9,11 +9,12 @@ import lombok.Setter;
 import org.springframework.cglib.core.Local;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 
-@EntityListeners(value = {AuditorAwareImpl.class}) // AuditorAwareImpl.class를 통해 생성, 수정 시간을 자동으로 업데이트
+@EntityListeners(value = {AuditingEntityListener.class}) // AuditorAwareImpl.class를 통해 생성, 수정 시간을 자동으로 업데이트
 @MappedSuperclass // 해당 클래스를 상속받는 클래스에게 속성만 제공
 @Getter
 @Setter
